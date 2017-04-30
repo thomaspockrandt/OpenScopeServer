89d7e58091061c62e504b11fc2db96b185fa18c0
Thomas Pockrandt <me@thomaspockrandt.com>
2017-04-30 11:09:59 +0200

    Change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OpenScopeServer' });
 });
 
 router.get('/sso', function(req, res, next) {
