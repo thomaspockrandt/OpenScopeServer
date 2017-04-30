9ad76e17a21a4e64e9d0c7c1ee9e93538cbcce57
Thomas Pockrandt <me@thomaspockrandt.com>
2017-04-30 10:14:48 +0200

    Add sample comments response

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ app.get('/', function (req, res) {
   res.send('OpenScopeServer');
 });
 
+app.get('/document/:uid/comments', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var json = [
+      {
+        "user": 12345,
+        "comment": "This looks interesting.",
+        "point": {"x": 2234, "y": 5431}
+      }, {
+        "user": 54321,
+        "comment": "What do you think?",
+        "point": {"x": 8564, "y": 4322}
+      }, {
+        "user": 3213,
+        "comment": "What do you think?",
+        "point": {"x": 8564, "y": 4322}
+      }
+    ];
+    res.send(JSON.stringify(json);
+});
+
 app.listen(3000, function () {
   console.log('OpenScopeServer listening on port 3000!');
 });
