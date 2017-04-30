d3811e3985864128d6c938beed84e7231c090779
Thomas Pockrandt <me@thomaspockrandt.com>
2017-04-30 10:45:42 +0200

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,40 @@ router.get('/sso', function(req, res, next) {
   res.json(json);
 });
 
+router.get('/document/:uid', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var json = {
+      "comments_count": 3
+    };
+    res.json(json);
+});
+
+router.get('/document/:uid/status', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var json = {
+      "status": 1
+    };
+    res.json(json);
+});
+
+router.get('/document/:uid/comments', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var json = [
+      {
+        "user": 12345,
+        "comment": "This looks interesting.",
+        "point": {"x": 2234, "y": 5431}
+      }, {
+        "user": 54321,
+        "comment": "What do you think?",
+        "point": {"x": 8564, "y": 4322}
+      }, {
+        "user": 3213,
+        "comment": "What do you think?",
+        "point": {"x": 12542, "y": 54231}
+      }
+    ];
+    res.json(json);
+});
+
 module.exports = router;
